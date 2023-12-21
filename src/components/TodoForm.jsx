0aa2dd106b9459e299ffc8ef7b29ed2305c7bd03
ramanboff <bayramofframan@gmail.com>
2023-12-22 00:11:01 +0400

    create value state input and handleSubmit function for submitting

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,5 +1,27 @@
-const TodoForm = () => {
-  return <form className="TodoForm">TodoForm</form>;
+import { useState } from "react";
+
+const TodoForm = ({addTodos}) => {
+  const [value, setValue] = useState("");
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodos(value)
+  };
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        className="todo-input"
+        placeholder="what is the task today?"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
 };
 
 export default TodoForm;
