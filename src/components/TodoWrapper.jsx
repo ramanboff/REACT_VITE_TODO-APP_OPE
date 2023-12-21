c8dad19e7cc35da24675f458421aa658ddda100b
ramanboff <bayramofframan@gmail.com>
2023-12-22 00:22:33 +0400

    passed addTodo function to TodoForm via props

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import TodoForm from "./TodoForm";
-import {v4 as uuidv4} from "uuid" 
+import { v4 as uuidv4 } from "uuid";
 
 const TodoWrapper = () => {
-
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTodos = (todo) => {
-    setTodos([...todos, {id:uuidv4(), task:todo, completed:false, editTodo:false}])
-  } 
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, editTodo: false },
+    ]);
+  };
   return (
     <div className="TodoWrapper">
-      <TodoForm addTodos={addTodos}/>
+      <TodoForm addTodos={addTodos} />
     </div>
   );
 };
