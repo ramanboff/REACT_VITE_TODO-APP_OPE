c41660419363294e7b686108219480ba6f4ec3b5
ramanboff <bayramofframan@gmail.com>
2023-12-22 01:23:06 +0400

    handleDelete function created for deleting each todo

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -17,12 +17,15 @@ const TodoWrapper = () => {
     setTodos(todos.map((todo) => todo.id === id? {...todo, completed:!todo.completed}: todo))
   }
 
+  const handleDelete =(id) => {
+    setTodos(todos.filter((todo) => todo.id !== id ))
+  }
  
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done!</h1>
       <TodoForm addTodos={addTodos} />
-      {todos.map((todo) => <Todo task={todo} key={todo.id} toggleComplete={toggleComplete}/>)}
+      {todos.map((todo) => <Todo task={todo} key={todo.id} toggleComplete={toggleComplete} handleDelete={handleDelete}/>)}
     </div>
   );
 };
