11e5882c4148af06d250061d40ea1ef49c414c7d
ramanboff <bayramofframan@gmail.com>
2023-12-22 00:48:19 +0400

    Each Todo component was used inside TodoWrapper component and sent task via props

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoForm from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
 
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -13,7 +14,9 @@ const TodoWrapper = () => {
   };
   return (
     <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
       <TodoForm addTodos={addTodos} />
+      {todos.map((todo) => <Todo task={todo.task} key={todo.id}/>)}
     </div>
   );
 };
