19f872ed12c2052bc9a4067d9c1c328036588856
ramanboff <bayramofframan@gmail.com>
2023-12-22 01:16:07 +0400

    toggleCompleted func passed to Todo component via props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare} from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-const Todo = ({task}) => {
+const Todo = ({task, toggleComplete}) => {
+
+
+
   return (
     <div className="Todo">
-      <p>{task}</p>
+      <p onClick={() => toggleComplete(task.id)} className={`${task.completed? "completed": ""}`}>{task.task}</p>
       <div>
         <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
         <FontAwesomeIcon className="delete-icon" icon={faTrash} />
