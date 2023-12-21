1a273dad1b9d435e4512387c8dcb314a277e008b
ramanboff <bayramofframan@gmail.com>
2023-12-22 00:48:44 +0400

    created one todo  for use

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,16 @@
-const Todo = () => {
-  return <div>Todo</div>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+const Todo = ({task}) => {
+  return (
+    <div className="Todo">
+      <p>{task}</p>
+      <div>
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
