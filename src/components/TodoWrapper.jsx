b68947154ba44df5e4ae2c1c89932ce8039a827c
ramanboff <bayramofframan@gmail.com>
2023-12-22 01:15:31 +0400

    toggleConplete Function created

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -12,11 +12,17 @@ const TodoWrapper = () => {
       { id: uuidv4(), task: todo, completed: false, editTodo: false },
     ]);
   };
+
+  const toggleComplete = (id) => {
+    setTodos(todos.map((todo) => todo.id === id? {...todo, completed:!todo.completed}: todo))
+  }
+
+ 
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done!</h1>
       <TodoForm addTodos={addTodos} />
-      {todos.map((todo) => <Todo task={todo.task} key={todo.id}/>)}
+      {todos.map((todo) => <Todo task={todo} key={todo.id} toggleComplete={toggleComplete}/>)}
     </div>
   );
 };
