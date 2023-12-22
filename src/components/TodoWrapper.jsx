e5f3651d0d51a1b97e9bc5d5f1b47535004711bb
ramanboff <bayramofframan@gmail.com>
2023-12-22 23:01:16 +0400

    create condition open todoForm or editTodoForm

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TodoForm from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
 
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -9,23 +10,55 @@ const TodoWrapper = () => {
   const addTodos = (todo) => {
     setTodos([
       ...todos,
-      { id: uuidv4(), task: todo, completed: false, editTodo: false },
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
     ]);
   };
 
   const toggleComplete = (id) => {
-    setTodos(todos.map((todo) => todo.id === id? {...todo, completed:!todo.completed}: todo))
-  }
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
-  const handleDelete =(id) => {
-    setTodos(todos.filter((todo) => todo.id !== id ))
-  }
- 
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  // editing
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done!</h1>
       <TodoForm addTodos={addTodos} />
-      {todos.map((todo) => <Todo task={todo} key={todo.id} toggleComplete={toggleComplete} handleDelete={handleDelete}/>)}
+
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm key={todo.id} editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={todo.id}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            editTodo={editTodo}
+          />
+        )
+      )}
     </div>
   );
 };
