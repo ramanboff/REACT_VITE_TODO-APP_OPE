e55400d3debd3fb9201aa4d508b2a658b9eba11d
ramanboff <bayramofframan@gmail.com>
2023-12-22 23:00:27 +0400

    added onClick function for editing todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare} from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-const Todo = ({task, toggleComplete, handleDelete}) => {
+const Todo = ({task, toggleComplete, handleDelete, editTodo}) => {
 
 
 
@@ -9,7 +9,7 @@ const Todo = ({task, toggleComplete, handleDelete}) => {
     <div className="Todo">
       <p onClick={() => toggleComplete(task.id)} className={`${task.completed? "completed": ""}`}>{task.task}</p>
       <div>
-        <FontAwesomeIcon  icon={faPenToSquare} className="edit-icon" />
+        <FontAwesomeIcon  icon={faPenToSquare} className="edit-icon" onClick={() => editTodo(task.id) } />
         <FontAwesomeIcon  icon={faTrash} className="delete-icon" onClick={() => handleDelete(task.id)} />
       </div>
     </div>
